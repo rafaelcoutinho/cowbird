d61d31266fdb63620408dd454cb83a1a0a67ca51
rafaelcoutinho <rafael.coutinho@gmail.com>
2015-09-07 23:19:51 -0300

    Notify of new SMS

--- app/src/main/java/com/dc/cowbird/service/IncomingSMS.java
+++ app/src/main/java/com/dc/cowbird/service/IncomingSMS.java
@@ -1,14 +1,20 @@
 package com.dc.cowbird.service;
 
+import android.app.NotificationManager;
+import android.app.PendingIntent;
 import android.content.BroadcastReceiver;
 import android.content.ContentResolver;
 import android.content.Context;
 import android.content.Intent;
+import android.net.Uri;
 import android.os.Bundle;
+import android.support.v4.app.NotificationCompat;
 import android.telephony.SmsMessage;
 import android.util.Log;
 
 import com.dc.cowbird.Constants;
+import com.dc.cowbird.MainControlActivity;
+import com.dc.cowbird.R;
 import com.dc.cowbird.parser.SMSParserFactory;
 import com.dc.cowbird.provider.ContentConstants;
 import com.dc.cowbird.vo.Protocol;
@@ -39,7 +45,30 @@ public class IncomingSMS extends BroadcastReceiver {
                 Protocol protocol = SMSParserFactory.getInstance(msgs[i].getTimestampMillis(), "", msgs[i].getMessageBody(), msgs[i].getOriginatingAddress());
                 if (protocol != null) {
                     try {
-                        cr.insert(ContentConstants.ProtocolURLs.URLProtocol.asURL(), protocol.toContentValues());
+                        Uri uri = cr.insert(ContentConstants.ProtocolURLs.URLProtocol.asURL(), protocol.toContentValues());
+                        NotificationCompat.Builder mBuilder =
+                                new NotificationCompat.Builder(context)
+                                        .setSmallIcon(R.drawable.ic_new_protocol)
+                                        .setContentTitle("Novo protocolo")
+                                        .setContentText("Identificamos um novo protocol! Clique aqui para adicionar mais informações a ele.");
+                        Intent resultIntent = new Intent(context, MainControlActivity.class);
+                        resultIntent.putExtra("uri", uri.getLastPathSegment());
+
+                        // Because clicking the notification opens a new ("special") activity, there's
+                        // no need to create an artificial back stack.
+                        PendingIntent resultPendingIntent =
+                                PendingIntent.getActivity(
+                                        context,
+                                        0,
+                                        resultIntent,
+                                        PendingIntent.FLAG_UPDATE_CURRENT
+                                );
+                        mBuilder.setContentIntent(resultPendingIntent);
+                        NotificationManager mNotifyMgr =
+                                (NotificationManager) context.getSystemService(Context.NOTIFICATION_SERVICE);
+// Builds the notification and issues it.
+                        mNotifyMgr.notify(213, mBuilder.build());
+
                     } catch (Exception e) {
                         Log.w(Constants.LOG_TAG, "Could not save SMS", e);
                     }
